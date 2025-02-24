067559585a90ac21253f74606ca2d98fbdf423f4
Navin Bhaskar <navin.bhaskar.5@gmail.com>
2025-02-24 18:11:10 +0530

    Fixed the test case

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,3 +1,18 @@
 
+from typing import List
+
+
 def fizz_buzz() -> str:
-    return ""
+    result_list: List[str] = []
+
+    for i in range(1, 101):
+        if i % 3 == 0:
+            result_list.append("Fizz")
+        elif i % 5 == 0:
+            result_list.append("Buzz")
+        elif i%3 == 0 and i % 5 == 0:
+            result_list.append("FizzBuzz")
+        else:
+            result_list.append(str(i))
+
+    return "\n".join(result_list)
